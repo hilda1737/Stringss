359db80396c182131f413f01a426e99c62d337b8
hilda1737 <hildamoraahh@gmail.com>
2022-03-02 15:59:08 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,35 @@
+fun main(){
+    var text = "akirachix"
+    var x=(text[0]) .toString() + (text[3]) + (text[4])
+    println(x)
+
+
+    var w = details( "Hilda" ,29)
+println(w)
+
+
+    var y=l("shilamoses")
+println( y)
+
+    detail("Hilda","ashlly")
+}
+fun details(x: String, Y: Int): String {
+    var name = x
+    var age = Y
+    var w = "Hi my name is $x i am $Y years old "
+    return w
+}
+fun l(x:String):Int{
+    return x.length
+}
+fun detail(Name:String,x:String) {
+    if (Name == x) {
+        println("That's me")
+    } else {
+        println("I dont know who that is")
+    }
+}
+
+
+
+
